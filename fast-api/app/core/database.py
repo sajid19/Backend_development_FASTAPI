86c3a1642b6f539563c7b90bcd8a462af173c426
sajid19 <sajidhasan2641@gmail.com>
2025-10-04 15:27:23 +0600

    update database mapping

--- fast-api/app/core/database.py
+++ fast-api/app/core/database.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 from sqlalchemy import create_engine
+from sqlmodel import  Session , SQLModel
+from typing import Annotated
+from fastapi import Depends
 
 BASE_DIR = Path(__file__).parent.parent.parent
 DATABASE_URL = f"sqlite:///{BASE_DIR / 'database' / 'user.db'}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
